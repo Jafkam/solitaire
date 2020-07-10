a3da3749eb4a73d7acf84f53c31fdfba5b631f19
Jafar <jafkam89@gmail.com>
2020-07-10 16:05:14 +0100

    format changes to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
 import Solitaire from "../Solitare/Solitaire";
 import Landing from "../Landing/Landing";
 
